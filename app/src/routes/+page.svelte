5c5ca470ebe8dd8aaf6745ff6cb73d2cae96dc2e
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-19 19:54:32 +0100

    save basic fetch but the problem is : Not implemented type annotation EmptyStatement     at handle_type_annotation (file:///home/laanas/2025/gitCodeCommitHistoryTransitionAnimation/app/node_modules/.pnpm/esrap@1.3.2/node_modules/esrap/src/handlers.js:560:10)     at Identifier (file:///home/laanas/2025/gitCodeCommitHistoryTransitionAnimation/app/node_modules/.pnpm/esrap@1.3.2/node_modules/esrap/src/handlers.js:1028:28)     at handle (file:///home/laanas/2025/gitCodeCommitHistoryTransitionAnimation/app/node_modules/.pnpm/esrap@1.3.2/node_modules/esrap/src/handlers.js:62:2)     at VariableDeclarator (file:///home/laanas/2025/gitCodeCommitHistoryTransitionAnimation/app/node_modules/.pnpm/esrap@1.3.2/node_modules/esrap/src/handlers.js:1531:3)     at handle (file:///home/laanas/2025/gitCodeCommitHistoryTransitionAnimation/app/node_modules/.pnpm/esrap@1.3.2/node_modules/esrap/src/handlers.js:62:2)     at handle_var_declaration (file:///home/laanas/2025/gitCodeCommitHistoryTransitionAnimation/app/node_modules/.pnpm/esrap@1.3.2/node_modules/esrap/src/handlers.js:319:3)     at VariableDeclaration (file:///home/laanas/2025/gitCodeCommitHistoryTransitionAnimation/app/node_modules/.pnpm/esrap@1.3.2/node_modules/esrap/src/handlers.js:1526:3)     at handle (file:///home/laanas/2025/gitCodeCommitHistoryTransitionAnimation/app/node_modules/.pnpm/esrap@1.3.2/node_modules/esrap/src/handlers.js:62:2)     at handle_body (file:///home/laanas/2025/gitCodeCommitHistoryTransitionAnimation/app/node_modules/.pnpm/esrap@1.3.2/node_modules/esrap/src/handlers.js:274:3)     at BlockStatement|ClassBody (file:///home/laanas/2025/gitCodeCommitHistoryTransitionAnimation/app/node_modules/.pnpm/esrap@1.3.2/node_modules/esrap/src/handlers.js:620:3 Click outside, press Esc key, or fix the code to dismiss. You can also disable this overlay by setting server.hmr.overlay to false in vite.config.ts.

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,3 +1,21 @@
-<script>
-  
+<script lang="ts">
+  const owner = "laaouatni";
+  const repoName = "arduino-2024-2025-prove";
+  const filePath = "december/componentsArduino/prova2/prova2.ino";
+
+  const apiUrlAllCommits = `https://api.github.com/repos/${owner}/${repoName}/commits?path=${filePath}`;
+
+  let shaCommitVersionCodes: string[] = [];
+
+  type TypeApiUrlAllCommits = {
+    sha: string
+  };
+
+  fetch(apiUrlAllCommits)
+    .then((response) => response.json())
+    .then((data: TypeApiUrlAllCommits[]) => {
+      data.forEach((thisCommit) => {
+        shaCommitVersionCodes.push(thisCommit.sha);
+      });
+    });
 </script>
