235973af5afe5d14241d14152cb40ec344838d0c
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-19 21:08:53 +0100

    now the code for fetching is written in the js function

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,31 +1,39 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  const repos = [
+    {
+      owner: "laaouatni",
+      repoName: "arduino-2024-2025-prove",
+      filePath: "december/componentsArduino/prova2/prova2.ino"
+    },
+    {
+      owner: "laaouatni",
+      repoName: "arduino-2024-2025-prove",
+      filePath: "/progettoLearningNovembre/laaEsp01/laaEsp01.ino"
+    }
+  ]
 
-  const owner = "laaouatni";
-  const repoName = "arduino-2024-2025-prove";
-  const filePath = "december/componentsArduino/prova2/prova2.ino";
-
-  const apiUrlAllCommits = `https://api.github.com/repos/${owner}/${repoName}/commits?path=${filePath}`;
-
-  let allShaCommitCodes: string[] = []
-  let allRawFileCodes: string[] = []
-
-  type TypeApiUrlAllCommits = {
-    sha: string
-  };
-
-  onMount(async () => {
+  async function fetchGithubAllCommitFiles(
+    owner: string,
+    repoName: string,
+    filePath: string,
+  ) {
+    type TypeApiUrlAllCommits = { sha: string };
+    const apiUrlAllCommits = `https://api.github.com/repos/${owner}/${repoName}/commits?path=${filePath}`;
     const fetchAllCommits = await fetch(apiUrlAllCommits);
     const commitsData: TypeApiUrlAllCommits[] = await fetchAllCommits.json();
-    const allShaCommitCodes = commitsData.map((thisCommit: TypeApiUrlAllCommits) => thisCommit.sha);
-    const allRawUrl = allShaCommitCodes.map((thisShaCode) => `https://raw.githubusercontent.com/${owner}/${repoName}/${thisShaCode}/${filePath}`);
-    const allRawCodeFileStrings = Promise.all(allRawUrl.map(async (thisRawUrl) => {
-      const fetchRawFile = await fetch(thisRawUrl);
-      return await fetchRawFile.text();
-    }));
-    allRawFileCodes = await allRawCodeFileStrings;
-
-    console.log(allRawFileCodes);
-  });
-
+    const allShaCommitCodes = commitsData.map(
+      (thisCommit: TypeApiUrlAllCommits) => thisCommit.sha,
+    );
+    const allRawUrl = allShaCommitCodes.map(
+      (thisShaCode) =>
+        `https://raw.githubusercontent.com/${owner}/${repoName}/${thisShaCode}/${filePath}`,
+    );
+    const allRawCodeFileStrings = await Promise.all(
+      allRawUrl.map(async (thisRawUrl) => {
+        const fetchRawFile = await fetch(thisRawUrl);
+        return await fetchRawFile.text();
+      }),
+    );
+    return allRawCodeFileStrings;
+  };
 </script>
