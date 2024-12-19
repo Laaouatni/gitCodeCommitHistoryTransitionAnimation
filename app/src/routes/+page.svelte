a8739839678a244ab515514a9367dc10ad8da06c
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-19 20:29:56 +0100

    solved by viewing https://github.com/sveltejs/vite-plugin-svelte/issues/1053#issuecomment-2550071312 so basically by using `pnpm up -L`

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,21 +1,29 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
   const owner = "laaouatni";
   const repoName = "arduino-2024-2025-prove";
   const filePath = "december/componentsArduino/prova2/prova2.ino";
 
   const apiUrlAllCommits = `https://api.github.com/repos/${owner}/${repoName}/commits?path=${filePath}`;
 
-  let shaCommitVersionCodes: string[] = [];
+  let shaCommitVersionCodes: string[] = $state([]);
 
   type TypeApiUrlAllCommits = {
     sha: string
   };
 
-  fetch(apiUrlAllCommits)
-    .then((response) => response.json())
-    .then((data: TypeApiUrlAllCommits[]) => {
-      data.forEach((thisCommit) => {
-        shaCommitVersionCodes.push(thisCommit.sha);
-      });
-    });
+  onMount(() => {
+    fetch(apiUrlAllCommits)
+      .then((response) => response.json())
+      .then((data: TypeApiUrlAllCommits[]) => {
+        data.forEach((thisCommit) => {
+          shaCommitVersionCodes = [...shaCommitVersionCodes, thisCommit.sha];
+        });
+      })
+  });
+
+  $effect(() => {
+    console.log(shaCommitVersionCodes);
+  });
 </script>
